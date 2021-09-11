34f8282111a3352700b6c40c45b10117d55933e5
wsielyfeng <learnroad@163.com>
2021-09-11 17:50:29 +0800

    实现批量引入svg

--- lib/index.tsx
+++ lib/index.tsx
@@ -3,4 +3,11 @@ import ReactDom from 'react-dom'
 import Icon from './icon';
 
 const root = document.getElementById('root')
-ReactDom.render(<Icon name="wechat"/>, root)
+ReactDom.render(<div>
+<Icon name="books"/>
+<Icon name="diamond"/>
+<Icon name="design"/>
+<Icon name="flower"/>
+<Icon name="light"/>
+<Icon name="wechat"/>
+</div>, root)
