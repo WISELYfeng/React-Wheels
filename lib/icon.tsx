f4aacd67b3eb12e512d75d167b892fb1e18dccf8
wsielyfeng <learnroad@163.com>
2021-09-13 22:47:28 +0800

    icon支持onClick

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -4,16 +4,15 @@ import './importIcons'
 
 interface IconProps{
     name: string;
+    onClick: React.MouseEventHandler<SVGElement>;
 }
 // Icon是一个FunctionComponent类型的组件，接收IconProps类型的参数
 const Icon: React.FunctionComponent<IconProps> = (props) => {
-    return (
-        <span>
-            <svg>
-                <use xlinkHref={`#${props.name}`}></use>
-            </svg> 
-        </span>
-    )
-}
+  return (
+    <svg onClick={props.onClick}>
+      <use xlinkHref={`#${props.name}`}/>
+    </svg>
+  );
+};
 
 export default Icon;
--- lib/index.tsx
+++ lib/index.tsx
@@ -2,12 +2,19 @@ import React from 'react'
 import ReactDom from 'react-dom'
 import Icon from './icon';
 
+const fn: React.MouseEventHandler = (e) => {
+    console.log(e)
+}
+
 const root = document.getElementById('root')
+// ReactDom.render(<div>
+// <Icon name="books"/>
+// <Icon name="diamond"/>
+// <Icon name="design"/>
+// <Icon name="flower"/>
+// <Icon name="light"/>
+// <Icon name="wechat"/>
+// </div>, root)
 ReactDom.render(<div>
-<Icon name="books"/>
-<Icon name="diamond"/>
-<Icon name="design"/>
-<Icon name="flower"/>
-<Icon name="light"/>
-<Icon name="wechat"/>
+    <Icon name="wechat" onClick={fn}/>
 </div>, root)
